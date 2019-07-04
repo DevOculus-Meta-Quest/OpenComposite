735774fcf745115dacf6db1d1b9d2a58b97b4931
ngorevsk <nick.gorevski@gmail.com>
2019-07-04 23:35:54 +0000

    Apply suggestion to OpenOVR/Reimpl/BaseInput.cpp

--- OpenOVR/Reimpl/BaseInput.cpp
+++ OpenOVR/Reimpl/BaseInput.cpp
@@ -44,7 +44,7 @@ std::string dirnameOf(const std::string& fname)
 {
 	size_t pos = fname.find_last_of("\\/");
 	return (std::string::npos == pos)
-		? ""
+		? "."
 		: fname.substr(0, pos);
 }
 bool iequals(const string& a, const string& b)
