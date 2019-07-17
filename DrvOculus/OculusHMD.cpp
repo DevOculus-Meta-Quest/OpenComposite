5c0325b40e47ba7aba99cb1ac27f3cb16779f0f4
Dmitry Galchinsky <galchinsky@gmail.com>
2019-07-17 08:15:29 +0300

    SDK 1.37.0 support

--- DrvOculus/OculusHMD.cpp
+++ DrvOculus/OculusHMD.cpp
@@ -142,10 +142,10 @@ bool OculusHMD::GetTimeSinceLastVsync(float * pfSecondsSinceLastVsync, uint64_t 
 }
 
 OVR_PUBLIC_FUNCTION(ovrResult)
-ovr_GetViewportStencil(
+ovr_GetFovStencil(
 		ovrSession session,
-		const ovrViewportStencilDesc* viewportStencilDesc,
-		ovrViewportStencilMeshBuffer* outMeshBuffer);
+		const ovrFovStencilDesc* fovStencilDesc,
+		ovrFovStencilMeshBuffer* outMeshBuffer);
 
 HiddenAreaMesh_t OculusHMD::GetHiddenAreaMesh(EVREye eEye, EHiddenAreaMeshType type) {
 	// TODO should we not cache this?
@@ -161,29 +161,29 @@ HiddenAreaMesh_t OculusHMD::GetHiddenAreaMesh(EVREye eEye, EHiddenAreaMeshType t
 	}
 
 	ovrEyeType eye = eEye == Eye_Left ? ovrEye_Left : ovrEye_Right;
-	ovrViewportStencilDesc desc;
+	ovrFovStencilDesc desc;
 	desc.Eye = eye;
 	desc.FovPort = ovr::hmdDesc.DefaultEyeFov[eye];
 	desc.HmdToEyeRotation = ovr::eyeRenderDesc[eye].HmdToEyePose.Orientation;
 
 	if (type == k_eHiddenAreaMesh_Inverse) {
-		desc.StencilType = ovrViewportStencil_VisibleArea;
+		desc.StencilType = ovrFovStencil_VisibleArea;
 	}
 	else if (type == k_eHiddenAreaMesh_LineLoop) {
-		desc.StencilType = ovrViewportStencil_BorderLine;
+		desc.StencilType = ovrFovStencil_BorderLine;
 	}
 	else {
-		desc.StencilType = ovrViewportStencil_HiddenArea;
+		desc.StencilType = ovrFovStencil_HiddenArea;
 	}
 
-	ovrViewportStencilMeshBuffer mb = { 0 };
+	ovrFovStencilMeshBuffer mb = { 0 };
 	mb.AllocVertexCount = 0;
 	mb.VertexBuffer = NULL;
 	mb.AllocIndexCount = 0;
 	mb.IndexBuffer = NULL;
 
 	// Query for the size
-	ovr_GetViewportStencil(*ovr::session, &desc, &mb);
+	ovr_GetFovStencil(*ovr::session, &desc, &mb);
 
 	// Create the buffers
 	mb.AllocVertexCount = mb.UsedVertexCount;
@@ -192,7 +192,7 @@ HiddenAreaMesh_t OculusHMD::GetHiddenAreaMesh(EVREye eEye, EHiddenAreaMeshType t
 	mb.IndexBuffer = new uint16_t[mb.AllocIndexCount];
 
 	// Get the data
-	ovr_GetViewportStencil(*ovr::session, &desc, &mb);
+	ovr_GetFovStencil(*ovr::session, &desc, &mb);
 
 	// Convert the data into something usable by SteamVR
 	HiddenAreaMesh_t &result = hiddenAreaMeshes[eEye];
