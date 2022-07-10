60fcdf33e39f4099b2cf6f62b3786d6997deadcb
Moses Turner <moses@collabora.com>
2022-07-10 21:05:31 +0100

    - Don't explode on upside-down images

--- OpenOVR/Compositor/vkcompositor.cpp
+++ OpenOVR/Compositor/vkcompositor.cpp
@@ -277,7 +277,7 @@ void VkCompositor::Invoke(XruEye eye, const vr::Texture_t* texture, const vr::VR
 
 		if (bounds.vMin > bounds.vMax) {
 			// TODO support vertically flipped images
-			XR_STUBBED(); // submitVerticallyFlipped = true;
+			// XR_STUBBED(); // submitVerticallyFlipped = true;
 			float newMax = bounds.vMin;
 			bounds.vMin = bounds.vMax;
 			bounds.vMax = newMax;
